29c642b435172ab5d7700c3ebcfa5ed228e9f11b
mkuo876@gmail.com <mkuo876@gmail.com>
2023-05-21 09:51:51 +0530

    removed unnecessary stuff

--- FrontEnd/notes/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-// axios.get("http://localhost:3001/notes").then((response) => {
-//   const notes = response.data;
-
-// });
-ReactDOM.createRoot(document.getElementById("root")).render(<App notes />);
